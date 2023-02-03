5165f5af6066ab46e11a0588e65436d132e0ba03
31core <im31core@yandex.com>
2023-02-03 22:56:07 +0800

    Add Code class.

--- web/events.ts
+++ web/events.ts
@@ -1,4 +1,4 @@
-var step = 0;
+var step: number = 0;
 
 function on_click() {
 	if(events_disabled) {
@@ -6,13 +6,13 @@ function on_click() {
 	}
 	/* 如果文字还没显示完全则显全 */
 	if(timers.length != 0) {
-		let saying = game_data[step - 1].split(" ")[1];
+		let saying = code_list[step - 1].args[0];
 		let character: string;
-		if(game_data[step - 1].split(" ").length == 2) {
+		if(code_list[step - 1].args.length == 1) {
 			character = "";
 		}
-		else if(game_data[step - 1].split(" ").length == 3) {
-			character = game_data[step - 1].split(" ")[2];
+		else if(code_list[step - 1].args.length == 2) {
+			character = code_list[step - 1].args[1];
 		}
 		dialog(character, saying);
 		/* 取消之前的计时器 */
@@ -25,10 +25,11 @@ function on_click() {
 	/* 自动存档 */
 	save("autosave", JSON.stringify({"step": step.toString(), 
 		"scene": get_url_arg("scene")}));
-	execute(game_data[step]);
+	execute(code_list[step]);
 	step++;
-	if(get_code_type(game_data[step - 1]) != "say" &&
-		get_code_type(game_data[step - 1]) != "choice") {
+
+	if(code_list[step - 1].type != "say" &&
+		code_list[step - 1].type != "choice") {
 		on_click();
 	}
 }
--- web/game.ts
+++ web/game.ts
@@ -1,9 +1,14 @@
-var game_data: string[];
+var code_list: Code[] = [];
 fetch("/script/" + get_url_arg("scene")).then((res) => {
 	return res.text()
 }).then((data) => {
 	data = data.replaceAll("\r\n", "\n");
-	game_data = data.split("\n");
+	data = data.replaceAll(/ +/g, " ");
+	data = data.replaceAll("\\n", "\n");
+	let lines = data.split("\n");
+	for(let i = 0; i < lines.length; i++) {
+		code_list.push(new Code(lines[i]));
+	}
 	/* restore */
 	if(get_url_arg("step") != "") {
 		step = Number.parseInt(get_url_arg("step"));
@@ -24,96 +29,83 @@ get_config("text-speed").then((data) => {
 });
 
 var music: HTMLAudioElement;
-var current_background: string;
-var current_music: string;
-var events_disabled = false;
+var current_background: string = "";
+var current_music: string = "";
+var events_disabled: boolean = false;
 
-var timers: number[];
-var figures: HTMLImageElement[];
+var timers: number[] = [];
+var figures: HTMLImageElement[] = [];
 
 var dialog_info = {
 	"character": "",
 	"saying": ""
 };
 
-function script_parse(str: string): string[] {
-	str = str.replaceAll(/ +/g, " ");
-	str = str.replaceAll("\\n", "\n");
-
-	let lis: string[];
-	let last = 0;
-	for(let i = 1; i < str.length; i++) {
-		if(str[i] == " " && str[i - 1] != "\\") {
-			lis.push(str.slice(last, i));
-			last = i + 1;
-		}
+class Code {
+	type: string;
+	args: string[];
+	constructor(code: string) {
+		this.type = code.split(" ")[0];
+		this.args = code.split(" ").slice(1);
 	}
-	lis.push(str.slice(last, str.length));
-
-	for(let i = 0; i < lis.length; i++) {
-		lis[i] = lis[i].replaceAll("\\ ", " ");
-	}
-	return lis;
 }
+
+
 
 /* Set dialog text */
 function dialog(character: string, saying: string) {
-	saying = saying.replaceAll("\n", "<br>");
-	document.getElementById("character").innerHTML = character;
-	document.getElementById("dialog-text").innerHTML = saying;
+	let len: number = 0;
+	dialog_info["character"] = character;
+	for(let i = 0; i < saying.length; i++) {
+		timers.push(setTimeout(() => {
+			timers = timers.slice(1);
+			saying = saying.replaceAll("\n", "<br>");
+			document.getElementById("character").innerHTML = character;
+			document.getElementById("dialog-text").innerHTML = saying.slice(0, len + 1);
+			len++;
+		}, speed * i));
+	}
+	dialog_info["saying"] = saying;
+	
 }
 
 /* execute script */
-function execute(code: string) {
-	let instructions = script_parse(code);
+function execute(code: Code) {
 	/* set dialog text */
-	if(instructions[0] == "say") {
-		let len = 0;
-		let saying = get_code_arg(code, 0);
-		let character: string;
-		if(instructions.length == 2) {
-			character = "";
-			dialog_info["character"] = "";
+	if(code.type == "say") {
+		if(code.args.length == 1) {
+			dialog("", code.args[0]);
 		}
-		else if(instructions.length == 3) {
-			character = instructions[2];
-			dialog_info["character"] = instructions[2];
+		else if(code.args.length == 2) {
+			dialog(code.args[1], code.args[0]);
 		}
-		for(let i = 0; i < saying.length; i++) {
-			timers.push(setTimeout(() => {
-				timers = timers.slice(1);
-				dialog(character, saying.slice(0, len + 1));
-				len++;
-			}, speed * i));
-		}
-		dialog_info["saying"] = instructions[1];
 	}
 	/* switch to the next scene */
-	else if(instructions[0] == "scene") {
-		location.assign("/game?scene=" + instructions[1]);
+	else if(code.type == "scene") {
+		location.assign("/game?scene=" + code.args[0]);
 	}
 	/* change background image */
-	else if(instructions[0] == "background") {
+	else if(code.type == "background") {
 		let background = document.getElementById("background");
 		background.setAttribute("style", "animation: background_disappearing 0.5s;");
 		background.addEventListener("animationend", () => {
-			set_background(instructions[1]);
+			set_background(code.args[0]);
 			background.setAttribute("style", "animation: background_appearing 0.5s;");
 		});
 	}
-	else if(instructions[0] == "play-sound") {
-		play_sound(instructions[1])
+	else if(code.type == "play-sound") {
+		play_sound(code.args[0])
 	}
-	else if(instructions[0] == "stop-sound") {
+	else if(code.type == "stop-sound") {
 		stop_sound();
 	}
 	/* 添加人物立绘 */
-	else if(instructions[0] == "figure") {
+	else if(code.type == "figure") {
 		let figures_div = document.getElementById("figures");
 		let figure = document.createElement("img");
 		figures.push(figure);
 
-		figure.setAttribute("src", "/data/" + instructions[1]);
+		figure.setAttribute("src", "/data/" + code.args[0]);
 
 		if(figures.length > 3) {
 			console.log("Warning: It's not a goot idea to add more than 3 figures.");
@@ -127,22 +119,22 @@ function execute(code: string) {
 		}
 		figures_div.appendChild(figure);
 	}
-	else if(instructions[0] == "clean-figure") {
+	else if(code.type == "clean-figure") {
 		for(let i = 0; i < figures.length; i++) {
 			figures[i].remove();
 		}
 		figures = [];
 	}
-	else if(instructions[0] == "choice") {
+	else if(code.type == "choice") {
 		events_disabled = true;
 		let choice: HTMLAnchorElement[];
 		document.getElementById("branch").removeAttribute("hidden");
-		for(let i = 1; i < instructions.length; i++) {
+		for(let i = 0; i < code.args.length; i++) {
 			choice.push(document.createElement("a"));
-			choice[i - 1].innerHTML = instructions[i].split(":")[0];
+			choice[i - 1].innerHTML = code.args[i].split(":")[0];
 
 			choice[i - 1].setAttribute("class", "button");
-			choice[i - 1].setAttribute("href", "/game?scene=" + instructions[i].split(":")[1]);
+			choice[i - 1].setAttribute("href", "/game?scene=" + code.args[i].split(":")[1]);
 			choice[i - 1].setAttribute("style", `text-decoration:none;
 				position:absolute;
 				left: 5%;
@@ -154,12 +146,14 @@ function execute(code: string) {
 	}
 }
 /* get code type */
-function get_code_type(code: string) {
-	return script_parse(code)[0];
+function get_code_type(code_str: string) {
+	let code: Code = new Code(code_str);
+	return code.type;
 }
 /* get code args */
-function get_code_arg(code: string, num: number) {
-	return script_parse(code)[num + 1];
+function get_code_arg(code_str: string, idx: number) {
+	let code: Code = new Code(code_str);
+	return code.args[idx];
 }
 
 /* Set title */
@@ -187,11 +181,11 @@ function stop_sound() {
 /* 通过step获取现在的状态 */
 function fastward() {
 	for(let i = 0; i < step; i++) {
-		if(get_code_type(game_data[i]) == "background") {
-			current_background = get_code_arg(game_data[i], 0);
+		if(code_list[i].type == "background") {
+			current_background = code_list[i].args[0];
 		}
-		else if(get_code_type(game_data[i]) == "play-sound") {
-			current_music = get_code_arg(game_data[i], 0);
+		else if(code_list[i].type == "play-sound") {
+			current_music = code_list[i].args[0];
 		}
 	}
 }
